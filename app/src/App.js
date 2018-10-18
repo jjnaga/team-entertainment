2dba084d92bf3654aa1de7bfc31da8d72d488f69
Jeremy Nagahama <naga@hawaii.edu>
2018-10-18 12:59:48 -1000

    Eject React and create files for mockup

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Menu } from "./components/Menu";
+import { Map } from "./components/Map";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-		  
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<Menu />
+				<Map />
+			</div>
+		);
+	}
 }
 
 export default App;
