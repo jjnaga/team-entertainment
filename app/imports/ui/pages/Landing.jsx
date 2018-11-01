4e8740a84457de551c9ab786aa2b612639677c10
VialliOu <vialli@hawaii.edu>
2018-11-01 00:35:42 -1000

    Options and Toolbox Completed

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Button, Checkbox, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+      <Grid columns={2} textAlign='center' container celled>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <h1>Map</h1>
           </Grid.Column>
+        </Grid.Row>
 
+        <Grid.Row>
           <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+            <h1>Toolbox</h1>
+              <Grid columns={5} celled>
+                <Grid.Column>
+                  <Image src="images/Tile 1.jpeg" size="medium" />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src="images/Tile 2.jpeg" size="medium" />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src="images/Tile 3.jpeg" size="medium" />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src="images/Tile 4.jpeg" size="medium" />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src="images/Tile 5.jpeg" size="medium" />
+                </Grid.Column>
+              </Grid>
+            <p>Drag tiles onto the map! Customize your Map!</p>
+            <Button fluid> Upload </Button>
           </Grid.Column>
+          <Grid.Column width={8}>
+            <h1>Options</h1>
+            <List>
+              <List.Item><Checkbox toggle label="Option 1" /></List.Item>
+              <List.Item><Checkbox toggle label="Option 2" /></List.Item>
+              <List.Item><Checkbox toggle label="Option 3" /></List.Item>
+              <List.Item><Checkbox toggle label="Option 4" /></List.Item>
+              <List.Item><Checkbox toggle label="Option 5" /></List.Item>
+              <List.Item><Checkbox toggle label="Option 6" /></List.Item>
+            </List>
+            <Button fluid> Generate </Button>
+          </Grid.Column>
+        </Grid.Row>
 
-        </Grid>
+      </Grid>
     );
   }
 }
