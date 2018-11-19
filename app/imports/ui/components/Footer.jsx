53bc5ee0c0e791aa5d34278f73415a72609994a2
Jeremy Nagahama <naga@hawaii.edu>
2018-11-18 15:54:40 -1000

    Add Leaflet-React to App, Add LeafMap component.
    LeafMap component is the component needed to add a  Leaflet map, it requires
    lat and lon paramaters. Leaflet is kinda wierd ngl.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
-  render() {
-    const divStyle = { paddingTop: '15px' };
-    return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              D&D Map Generator
-          </div>
-        </footer>
-    );
-  }
+	render() {
+		const divStyle = { paddingTop: "15px" };
+		return (
+			<footer>
+				<div style={divStyle} className="ui center aligned container">
+					<hr />
+					D&D Map Generator
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
